b4035960875496e4f5c12d2a122a0f1758f8fd47
David Bouscarle <d.bouscarle@gmail.com>
2021-02-21 22:49:32 +0100

    hover li / work on header turn black

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const   menuResponsive  = document.querySelector('.menu__responsive'),
+const   header          = document.querySelector('.header'),
+        menuResponsive  = document.querySelector('.menu__responsive'),
         burger          = document.querySelector('.header__nav__burger'),
         closeToggle     = document.querySelector('.menu__responsive__toggle');
 
 // EVENT LISTENERS
 
+header.addEventListener('scroll', turnBlack);
 burger.addEventListener('click', menuOpener);
 closeToggle.addEventListener('click', menuCloser);
 
 // FUNCTIONS
+
+function turnBlack() {
+    if(window.scrollY > 0) {
+        header.classList.add('header__turn__black');
+    } else {
+        header.classList.remove('header__turn__black');
+    }
+}
 
 function menuOpener() {
     menuResponsive.classList.add('menu__responsive__open');
