9076428ec89607724895da0cbc1f6ab663e6c921
David Bouscarle <d.bouscarle@gmail.com>
2021-02-22 16:22:49 +0100

    add the curtain effect

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const   header          = document.querySelector('.header'),
+        curtain         = document.querySelector('.curtain'),
         menuResponsive  = document.querySelector('.menu__responsive'),
         burger          = document.querySelector('.header__nav__burger'),
         closeToggle     = document.querySelector('.menu__responsive__toggle');
@@ -21,8 +22,12 @@ function turnBlack() {
 
 function menuOpener() {
     menuResponsive.classList.add('menu__responsive__open');
+    curtain.classList.add('menu__responsive__open');
 }
 
 function menuCloser() {
     menuResponsive.classList.remove('menu__responsive__open');
+    setTimeout(() => {
+        curtain.classList.remove('menu__responsive__open');
+    }, 500);   
 }
