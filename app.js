088092e3a7bad09b963554b8b6bea554f97c2272
David Bouscarle <d.bouscarle@gmail.com>
2021-02-22 16:57:51 +0100

    add scroll effect header

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-const   header          = document.querySelector('.header'),
-        curtain         = document.querySelector('.curtain'),
-        menuResponsive  = document.querySelector('.menu__responsive'),
-        burger          = document.querySelector('.header__nav__burger'),
-        closeToggle     = document.querySelector('.menu__responsive__toggle');
+const   header          = document.querySelector(".header"),
+        curtain         = document.querySelector(".curtain"),
+        menuResponsive  = document.querySelector(".menu__responsive"),
+        burger          = document.querySelector(".header__nav__burger"),
+        closeToggle     = document.querySelector(".menu__responsive__toggle");
 
 // EVENT LISTENERS
 
-header.addEventListener('scroll', turnBlack);
-burger.addEventListener('click', menuOpener);
-closeToggle.addEventListener('click', menuCloser);
+window.addEventListener("scroll", turnBlack);
+burger.addEventListener("click", menuOpener);
+closeToggle.addEventListener("click", menuCloser);
 
 // FUNCTIONS
 
 function turnBlack() {
-    if(window.scrollY > 0) {
-        header.classList.add('header__turn__black');
-    } else {
-        header.classList.remove('header__turn__black');
-    }
+    header.classList.toggle("header__turn__black", window.scrollY > 0);
 }
 
 function menuOpener() {
-    menuResponsive.classList.add('menu__responsive__open');
-    curtain.classList.add('menu__responsive__open');
+    menuResponsive.classList.add("menu__responsive__open");
+    curtain.classList.add("menu__responsive__open");
 }
 
 function menuCloser() {
-    menuResponsive.classList.remove('menu__responsive__open');
+    menuResponsive.classList.remove("menu__responsive__open");
     setTimeout(() => {
-        curtain.classList.remove('menu__responsive__open');
-    }, 500);   
+        curtain.classList.remove("menu__responsive__open");
+    }, 500);
 }
